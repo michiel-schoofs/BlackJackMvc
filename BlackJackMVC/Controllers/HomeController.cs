4c23e4c2e4d1ea52087a476ccfdf9f80c3ee9ba3
Michiel Schoofs <michiel.schoofs@student.hogent.be>
2019-01-07 12:06:53 +0100

    Added Index View en Home Controller

---
+++ BlackJackMVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using BlackJackMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackJackMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BlackJack _blackJack;
+
+        public IActionResult Index()
+        {
+            _blackJack = new BlackJack();
+            return View(_blackJack);
+        }
+    }
+}
--- BlackJackMVC/Startup.cs
+++ BlackJackMVC/Startup.cs
@@ -12,6 +12,7 @@ namespace BlackJackMVC {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,8 +21,13 @@ namespace BlackJackMVC {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
